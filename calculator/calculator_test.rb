de1289a2435057ebbf04843ca9a7eafa30c6473e
Tamaki MATSUMOTO <matsumoto@timedia.co.jp>
2013-03-08 10:42:49 +0900

    クラスメソッドをやめたのに追随

--- calculator/calculator_test.rb
+++ calculator/calculator_test.rb
@@ -1,12 +1,20 @@
 require 'test/unit'
 require_relative 'calculator'
 
+class TestCalculator
+  include Calculator::Interest
+end
+
 class InterestCalclatorTest < Test::Unit::TestCase
+    def setup
+        @interest = TestCalculator.new
+    end
+
     def test_parse1
         from = Date.new(2012,12,10)
         to = Date.new(2013,1,20)
 
-        terms = Calculator::Interest.terms_by_cutoff_day(from, to, 27)
+        terms = @interest.terms_by_cutoff_day(from, to, 27)
         terms.each do |t|
             p t
         end
@@ -16,7 +24,7 @@ class InterestCalclatorTest < Test::Unit::TestCase
         from = Date.new(2012,12,10)
         to = Date.new(2013,3,20)
 
-        terms = Calculator::Interest.terms_by_cutoff_day(from, to, 27)
+        terms = @interest.terms_by_cutoff_day(from, to, 27)
         terms.each do |t|
             p t
         end
@@ -26,7 +34,7 @@ class InterestCalclatorTest < Test::Unit::TestCase
         from = Date.new(2012,12,10)
         to = Date.new(2013,3,20)
 
-        terms = Calculator::Interest.terms_by_end_of_month(from, to)
+        terms = @interest.terms_by_end_of_month(from, to)
         terms.each do |t|
             p t
         end
@@ -43,7 +51,7 @@ class InterestCalclatorTest < Test::Unit::TestCase
         min_date = Date.new(2013,2,1)
 
         p "****"
-        terms = Calculator::Interest.terms_by_end_of_month(from, to)
+        terms = @interest.terms_by_end_of_month(from, to)
         terms.select{ |e| e.last >= min_date }.each do |e|
             p e
         end
@@ -55,8 +63,8 @@ class InterestCalclatorTest < Test::Unit::TestCase
         from = Date.new(2012,12,10)
         to = Date.new(2013,1,27)
 
-        p Calculator::Interest.calc_leap(principal, from, to, rate)
-        p Calculator::Interest.calc_365(principal, from, to, rate)
+        p @interest.calc_leap(principal, from, to, rate)
+        p @interest.calc_365(principal, from, to, rate)
     end
 
     def test_calc_leap2
@@ -65,10 +73,10 @@ class InterestCalclatorTest < Test::Unit::TestCase
         from = Date.new(2012,12,10)
         to = Date.new(2012,12,31)
 
-        p Calculator::Interest.calc(
-            Calculator::Interest.both_end_base(principal, from, to), 366, rate)
-        p Calculator::Interest.calc(
-            Calculator::Interest.both_end_base(principal, from, to), 365, rate)
+        p @interest.calc(
+            @interest.both_end_base(principal, from, to), 366, rate)
+        p @interest.calc(
+            @interest.both_end_base(principal, from, to), 365, rate)
     end
 
     def test_calc_leap3
@@ -77,10 +85,10 @@ class InterestCalclatorTest < Test::Unit::TestCase
         from = Date.new(2013,1,1)
         to = Date.new(2013,1,27)
 
-        doy = Calculator::Interest.days_of_year(to)
+        doy = @interest.days_of_year(to)
 
-        p Calculator::Interest.calc(
-            Calculator::Interest.one_end_base(principal, from, to), 365, rate)
+        p @interest.calc(
+            @interest.one_end_base(principal, from, to), 365, rate)
     end
 
     def test_calc1
@@ -89,11 +97,11 @@ class InterestCalclatorTest < Test::Unit::TestCase
         from = Date.new(2011,10,8)
         to = Date.new(2012,4,20)
 
-        terms = Calculator::Interest.terms_by_cutoff_day(from, to, 27)
+        terms = @interest.terms_by_cutoff_day(from, to, 27)
         a = 0
         p "----"
         terms.each do |t|
-            result = Calculator::Interest.calc_leap(principal, t.first, t.last, rate).truncate
+            result = @interest.calc_leap(principal, t.first, t.last, rate).truncate
             a += result
             p [t.first.to_s, t.last.to_s, (t.last - t.first).to_i, result]
         end
@@ -108,7 +116,7 @@ class InterestCalclatorTest < Test::Unit::TestCase
         to = Date.new(2012,4,20)
 
         p "===="
-        p Calculator::Interest.calc_leap(principal, from, to, rate).truncate
+        p @interest.calc_leap(principal, from, to, rate).truncate
         p "===="
     end
 
@@ -118,11 +126,11 @@ class InterestCalclatorTest < Test::Unit::TestCase
         from = Date.new(2011,10,8)
         to = Date.new(2012,4,20)
 
-        terms = Calculator::Interest.terms_by_end_of_month(from, to)
+        terms = @interest.terms_by_end_of_month(from, to)
         a = 0
         p "----"
         terms.each do |t|
-            result = Calculator::Interest.calc_leap(principal, t.first, t.last, rate).truncate
+            result = @interest.calc_leap(principal, t.first, t.last, rate).truncate
             a += result
             p [t.first.to_s, t.last.to_s, (t.last - t.first).to_i, result]
         end
